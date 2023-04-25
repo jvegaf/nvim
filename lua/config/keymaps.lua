753598c4cd15317e48d228a4002078486759c4f4
Jose Vega <josevega234@gmail.com>
2023-04-25 22:03:32 +0200

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,38 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local NS = { noremap = true, silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+keymap("n", "<C-i>", "<C-i>", NS)
+
+keymap("n", "x", '"_x', NS)
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Cancel search highlighting with ESC
+keymap("n", "<ESC><ESC>", ":nohlsearch<Bar>:echo<CR>", NS)
+
+-- Select all
+keymap("n", "<C-a>", "gg<S-v>G", NS)
+
+keymap("n", "vv", "V", NS)
+keymap("n", "<leader>o", "o<Esc>", { noremap = true, silent = true, desc = "Insert line down" })
+keymap("n", "<leader>O", "O<Esc>", { noremap = true, silent = true, desc = "Insert line up" })
+-- keymap("n", "<leader>p", "o<Esc>p", NS)
+keymap("n", "<leader>p", "<cmd>pu<cr>", { noremap = true, silent = true, desc = "Paste underline" })
+keymap("n", "<leader>P", "<cmd>pu!<cr>", { noremap = true, silent = true, desc = "Paste overline" })
+
+-- dont yank on visual paste
+keymap("v", "p", '"_dP', NS)
+
+keymap("n", "<F1>", "<cmd>vertical help<cr>", { noremap = true, silent = true, desc = "Vertical Help" })
