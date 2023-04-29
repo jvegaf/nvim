7fd443ce26e67a3a79a770784e52b8e05c9c296d
Jose Vega <josevega234@gmail.com>
2023-04-29 05:39:38 +0200

    leader ,

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -99,6 +99,8 @@ vim.cmd("set whichwrap+=<,>,[,],h,l")
 vim.cmd([[set iskeyword+=-]])
 vim.cmd([[set formatoptions-=cro]]) -- TODO: this doesn't seem to work
 
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 vim.filetype.add({
   extension = {
     conf = "dosini",
