543c30cd4273bbb2ae5bee3f3aa0ff1f91d349eb
Jose Vega <jevf@gft.com>
2023-05-09 14:42:38 +0200

    ufo

--- lua/plugins/configs/nvim-ufo.lua
+++ lua/plugins/configs/nvim-ufo.lua
@@ -31,10 +31,10 @@ local handler = function(virtText, lnum, endLnum, width, truncate)
   return newVirtText
 end
 
-vim.o.foldcolumn = "1" -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
+-- vim.o.foldcolumn = "1" -- '0' is not bad
+-- vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+-- vim.o.foldlevelstart = 99
+-- vim.o.foldenable = true
 
 ufo.setup({
   fold_virt_text_handler = handler,
