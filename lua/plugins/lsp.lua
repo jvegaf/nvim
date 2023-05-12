9fd8644b51f65269773513346b47de5558800019
Jose Vega <josevega234@gmail.com>
2023-05-12 18:00:42 +0200

    schemastore

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,30 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    "b0o/schemastore.nvim",
+  },
   ---@class PluginLspOpts
   opts = {
     ---@type lspconfig.options
     servers = {
       -- pyright will be automatically installed with mason and loaded with lspconfig
       pyright = {},
+      yamlls = {
+        settings = {
+          yaml = {
+            schemas = require("schemastore").yaml.schemas(),
+            validate = true,
+          },
+        },
+      },
+      jsonls = {
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      },
     },
   },
 }
