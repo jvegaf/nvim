2e98d305a3e4c8507b63e031d5a148f12fa1bb18
Jose Vega <josevega234@gmail.com>
2023-07-03 03:32:19 +0200

    codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,48 +1,36 @@
--- local codeium_wsl = {
---   "hrsh7th/nvim-cmp",
---   dependencies = {
---     {
---       "jcdickinson/codeium.nvim",
---       lazy = false,
---       dependencies = {
---         "nvim-lua/plenary.nvim",
---         "hrsh7th/nvim-cmp",
---       },
---       config = function()
---         require("codeium").setup({})
---       end,
+-- return {
+--   {
+--     "jcdickinson/codeium.nvim",
+--     dependencies = {
+--       "nvim-lua/plenary.nvim",
+--       "hrsh7th/nvim-cmp",
 --     },
+--     -- event = "InsertEnter",
+--     cmd = { "Codeium" },
+--     config = function()
+--       require("codeium").setup {}
+--     end,
 --   },
---   ---@diagnostic disable-next-line: undefined-doc-name
---   ---@param opts cmp.ConfigSchema
---   opts = function(_, opts)
---     local cmp = require("cmp")
---     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "codeium" } }))
---   end,
+--   {
+--     "hrsh7th/nvim-cmp",
+--     dependencies = {
+--       "jcdickinson/codeium.nvim",
+--     },
+--     config = function(_, opts)
+--       local cmp = require "cmp"
+--       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "codeium" } }))
+--     end,
+--   },
 -- }
-
-local codeium_default = {
-  "Exafunction/codeium.vim",
+return {
+  'Exafunction/codeium.vim',
   lazy = false,
-  -- cmd = "Codeium",
-  event = "InsertEnter",
-    -- stylua: ignore
   config = function ()
     vim.g.codeium_disable_bindings = 1
-    vim.keymap.set("i", "<A-l>", function() return vim.fn["codeium#Accept"]() end, { noremap = true, expr = true, desc = "Codeium Accept" })
-    vim.keymap.set("i", "<A-.>", function() return vim.fn["codeium#CycleCompletions"](1) end, { noremap = true, expr = true, desc = "Codeium Cycle next" })
-    vim.keymap.set("i", "<A-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { noremap = true, expr = true, desc = "Codeium Cycle prev" })
-    vim.keymap.set("i", "<A-q>", function() return vim.fn["codeium#Clear"]() end, { noremap = true, expr = true, desc = "Codeium Clear" })
-    vim.keymap.set("i", "<A-s>", function() return vim.fn["codeium#Complete"]() end, { noremap = true, expr = true, desc = "Codeium Complete" })
-  end,
+
+    vim.keymap.set('i', '<A-l>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+    vim.keymap.set('i', '<A-j>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+    vim.keymap.set('i', '<A-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+    vim.keymap.set('i', '<A-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+  end
 }
-
--- local is_lin = vim.loop.os_uname().sysname == "Linux"
---
--- local is_wsl = is_lin and vim.loop.os_uname().release:lower():find("microsoft") and true or false
---
--- if is_wsl then
---   return codeium_wsl
--- end
-
-return codeium_default
