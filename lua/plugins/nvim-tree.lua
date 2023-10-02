1bd6ff5e28bd87b6fb5336356c81693cc120baf9
Jose Vega <jevf@gft.com>
2023-10-02 15:57:13 +0200

    nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,7 +8,7 @@ return {
     cmd = { "NvimTreeToggle", "NvimTreeFocus" },
     keys = {
       { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree Toggle" },
-      { "<leader>o", "<cmd>NvimTreeFocus<cr>", desc = "NvimTree Focus" },
+      { "<leader>o", "<cmd>NvimTreeFocus<cr>",  desc = "NvimTree Focus" },
     },
     opts = {
       diagnostics = {
@@ -68,5 +68,30 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("nvim-tree").setup(opts)
+
+      -- local api = require("nvim-tree.api")
+      -- api.events.subscribe(api.events.Event.FileCreated, function(file)
+      --   vim.cmd("edit " .. file.fname)
+      -- end)
+      --
+      vim.api.nvim_create_autocmd("QuitPre", {
+        callback = function()
+          local invalid_win = {}
+          local wins = vim.api.nvim_list_wins()
+          for _, w in ipairs(wins) do
+            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+            if bufname:match("NvimTree_") ~= nil then
+              table.insert(invalid_win, w)
+            end
+          end
+          if #invalid_win == #wins - 1 then
+            -- Should quit, so we close all invalid windows.
+            for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
+          end
+        end
+      })
+    end,
   },
 }
