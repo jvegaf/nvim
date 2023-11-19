c14039c72f68af8cc191fea1d108650337836645
Jose Vega <josevega234@gmail.com>
2023-11-19 23:53:23 +0100

    lsp fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,7 +58,6 @@ return {
     dependencies = {
       {
         -- LSP Support
-        { "neovim/nvim-lspconfig" },
         { "williamboman/mason.nvim" },
         { "williamboman/mason-lspconfig.nvim" },
 
@@ -138,4 +137,28 @@ return {
       })
     end,
   },
+  {
+    'neovim/nvim-lspconfig',
+    cmd = 'LspInfo',
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+    },
+    config = function()
+      -- This is where all the LSP shenanigans will live
+      require('neoconf').setup()
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
+
+      lsp_zero.on_attach(function(client, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+
+      -- (Optional) Configure lua language server for neovim
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end
+  }
 }
