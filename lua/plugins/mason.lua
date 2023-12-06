2da5ba81e1caacf87ecb2e9f6efdb8a0edf82230
Jose Vega <josevega234@gmail.com>
2023-12-06 23:24:04 +0100

    auto install

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,12 +1,31 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "shellcheck",
-      "shfmt",
-      "flake8",
-      "prettierd",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "prettierd",
+      },
     },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      automatic_installation = true,
+    },
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = nil,
+        automatic_installation = true,
+      })
+    end,
   },
 }
