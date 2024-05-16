8bf917e680c41516fa4d757a4f65f4c2d938f1e8
Jose <th3g3ntl3man@Joses-MacBook-Pro.local>
2024-05-16 12:32:30 +0200

    darwin

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ require("config.lazy")
 
 local is_lin = vim.loop.os_uname().sysname == "Linux"
 local is_win = vim.loop.os_uname().sysname == "Windows_NT"
+local is_mac = vim.loop.os_uname().sysname == "Darwin"
 
 if is_lin then
   require("config.linux")
@@ -12,3 +13,7 @@ end
 if is_win then
   require("config.windows")
 end
+
+-- if is_mac then
+--   print("is mac")
+-- end
