03878af13f0304d7991cbf30263ef372db4bf8d4
Jose Vega <josevega234@gmail.com>
2024-09-07 18:03:05 +0200

    plastformio

---
+++ lua/plugins/platformio.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "tpope/vim-dispatch",
+    cmd = {
+      "AbortDispatch",
+      "Copen",
+      "Dispatch",
+      "Focus",
+      "FocusDispatch",
+      "Make",
+      "Spawn",
+      "Start",
+    },
+    config = function()
+      vim.g.dispatch_no_maps = 1
+    end,
+  },
+
+  {
+    "normen/vim-pio",
+    enabled = false,
+    cmd = {
+      "PIO",
+      "PIOInit",
+      "PIOInstall",
+      "PIOUninstall",
+      "PIONewProject",
+      "PIOAddLibrary",
+      "PIORemoveLibrary",
+    },
+  },
+
+  {
+    "anurag3301/nvim-platformio.lua",
+    dependencies = {
+      { "akinsho/nvim-toggleterm.lua" },
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    cmd = {
+      "Pioinit",
+      "Piorun",
+      "Piocmd",
+      "Piolib",
+      "Piomon",
+    },
+  },
+
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    ft = { "c", "cpp" },
+    opts = {
+      extensions = {
+        inlay_hints = vim.fn.has("nvim-0.10") == 1,
+      },
+    },
+  },
+
+  {
+    "RaafatTurki/hex.nvim",
+    -- enabled = vim.fn.executable("xxd") == 1,
+    enabled = false,
+    config = true,
+    cmd = { "HexDump", "HexAssemble", "HexToggle" },
+  },
+
+  {
+    "jedrzejboczar/nvim-dap-cortex-debug",
+    enabled = false,
+    dependencies = { "mfussenegger/nvim-dap" },
+    config = true,
+  },
+}
