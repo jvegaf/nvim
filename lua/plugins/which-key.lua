0167e646343be003567373e76c78abb75400df36
Jose Vega <josevega234@gmail.com>
2024-09-11 18:59:29 +0200

    which key fixes

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,13 +3,10 @@ return {
   config = function()
     local wk = require("which-key")
     wk.add({
-      ["<leader>r"] = { group = "+Refactor" },
-      ["<leader>i"] = { group = "+AnyJump" },
-      ["<leader>z"] = { group = "+System" },
-      ["<leader>g"] = {
-        -- require install gitui   https://github.com/extrawurst/gitui
-        t = { "<cmd>lua require('utils.term').git_client_toggle()<CR>", "Git TUI" },
-      },
+      { "<leader>r", group = "+Refactor" },
+      { "<leader>i", group = "+AnyJump" },
+      { "<leader>z", group = "+System" },
+      { "<leader>gt", "<cmd>lua require('utils.term').git_client_toggle()<CR>", desc = "Git TUI" },
     })
   end,
 }
