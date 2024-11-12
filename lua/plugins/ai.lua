186aa79abca6196480d596a2baa2116fb92c17bb
Jose Vega <josevega234@gmail.com>
2024-11-12 06:11:44 +0100

    neocodeium

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,7 +3,6 @@ return {
     "monkoose/neocodeium",
     event = "VeryLazy",
     -- enabled = vim.fn.has("nvim-0.10") == 1,
-    enabled = false,
     config = function()
       local neocodeium = require("neocodeium")
       neocodeium.setup()
@@ -11,13 +10,13 @@ return {
       vim.keymap.set("i", "<A-w>", function()
         require("neocodeium").accept_word()
       end)
-      vim.keymap.set("i", "<A-a>", function()
+      vim.keymap.set("i", "<A-l>", function()
         require("neocodeium").accept_line()
       end)
-      vim.keymap.set("i", "<A-e>", function()
+      vim.keymap.set("i", "<A-j>", function()
         require("neocodeium").cycle_or_complete()
       end)
-      vim.keymap.set("i", "<A-r>", function()
+      vim.keymap.set("i", "<A-k>", function()
         require("neocodeium").cycle_or_complete(-1)
       end)
       vim.keymap.set("i", "<A-c>", function()
@@ -27,6 +26,7 @@ return {
   },
   {
     "supermaven-inc/supermaven-nvim",
+    enabled = false,
     event = "VeryLazy",
     config = function()
       require("supermaven-nvim").setup({
